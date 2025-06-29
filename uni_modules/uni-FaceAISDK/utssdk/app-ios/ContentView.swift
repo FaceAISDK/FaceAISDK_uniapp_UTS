1e11e01114f607bad43c6b41291b23d7858f7a97
AnyLife Air <anylife.zlb@gmail.com>
2025-06-29 20:29:42 +0800

    更新原生SDK

---
+++ uni_modules/uni-FaceAISDK/utssdk/app-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FaceAISDK_Core
+//
+//  Created by anylife on 2025/5/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("UTS 插件验证，无功能!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ uni_modules/uni-FaceAISDK/utssdk/app-ios/SwiftUINavigationDelegate.swift
@@ -0,0 +1,14 @@
+// // app-ios/index.uts
+// import { UIViewController, UIHostingController } from "UIKit";
+
+// class SwiftUINavigationDelegate {
+//     var callback: (result: NavigationResult) => void;
+    
+//     constructor(callback: (result: NavigationResult) => void) {
+//         this.callback = callback;
+//     }  
+    
+//     didFinishWithResult(result: NavigationResult) {
+//         this.callback(result);
+//     } 
+// }
