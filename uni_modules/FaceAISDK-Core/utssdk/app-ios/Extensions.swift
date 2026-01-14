0a2a2b1e14577414f34a78dc64b45cdca8724afa
FaceSDK <FaceAISDK.Service@gmail.com>
2026-01-14 21:18:23 +0800

    升级SDK

--- uni_modules/FaceAISDK-Core/utssdk/app-ios/Extensions.swift
+++ uni_modules/FaceAISDK-Core/utssdk/app-ios/Extensions.swift
@@ -2,8 +2,8 @@ import SwiftUI
 
 // 1. 扩展 Color 类
 extension Color {
-    // 2. 定义静态属性 faceMain
-    // 直接使用 RGB 数值 (对应 #0B4D46)
+    // 2. 定义静态属性主题颜色 faceMain
+    // 直接使用 RGB 数值 (对应 #0B4D46，不会算的用AI辅助计算一下)
     static var faceMain: Color {
         return Color(red: 11 / 255.0, green: 77 / 255.0, blue: 70 / 255.0)
     }
